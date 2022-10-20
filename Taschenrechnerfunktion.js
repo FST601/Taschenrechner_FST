cdf9b8a02cf11d1afa8da2ad7fa4676740199c2e
fst601 <finn.stawitzki@itc-studenten.de>
2022-10-20 21:01:29 +0200

    commit

---
+++ Taschenrechnerfunktion.js
@@ -0,0 +1,32 @@
+const Anzeige = document.getElementById("Anzeige");
+
+let Wert1 = document.getElementById();
+console.log(Wert1);
+
+function CE() {
+    this.Anzeige.innerText = "";
+}
+
+function plus(){
+    this.Anzeige.innerText = "+";
+}
+
+function minus(){
+    this.Anzeige.innerText = "-";
+}
+
+function geteilt(){
+    this.Anzeige.innerText = "/";
+}
+
+function mal(){
+    this.Anzeige.innerText = "*";
+}
+
+function wurzel(){
+    this.Anzeige.innerText = "√";
+}
+
+function potenz(){
+    this.Anzeige.innerText = "^";
+}
